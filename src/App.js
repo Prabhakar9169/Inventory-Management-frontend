dfbd581b24acd567fd126621a52d4b00a37c9895
Prabhakar <prabhakar9169@gmail.com>
2022-12-16 19:18:42 +0530

    first commir

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Signup from "./Autentication/Signup";
+import Login from "./Autentication/Login";
+import { BrowserRouter , Routes , Route } from "react-router-dom";
+import Home from "./Home";
+import Dashboard from "./components/Dashboard";
+import User from "./components/User";
+import Auth from "./Autentication/Auth";
+import Product from "./components/Product";
+import Customers from "./components/Customers";
+import Order from "./components/Order";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route exact path = '/' element= {<Home/>}/>
+        <Route path="/login"   element = {<Login/>}/>
+        <Route path="/signup"   element = {<Signup/>}/>
+        <Route path="/dashboard"   element = {<Auth><Dashboard/></Auth>}/>
+        <Route path="/user"   element = {<Auth><User/></Auth>}/>
+        <Route path="/product"   element = {<Auth><Product/></Auth>}/>
+        <Route path="/order"   element = {<Auth><Order/></Auth>}/>
+        <Route path="/customer"   element = {<Auth><Customers/></Auth>}/>
+       
+      </Routes>
+      </BrowserRouter>
+     
     </div>
   );
 }
